cb2c5ace4d22a2733b04dc4d5f54b6de811d47e4
g-popovic <georgepopovich14@gmail.com>
2025-07-19 13:53:14 +0200

    Refine about us section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,26 @@
 "use client";
 
 import "./About.css";
+import Image from "next/image";
+import aboutImage from "../../public/about-2.jpg";
 
 export default function About() {
 	return (
 		<section id='about' className='about'>
-			<div className='section-start-blur' />
-
 			<div className='container'>
 				<h2>O nama</h2>
-				<p>TODO</p>
+
+				<div className='about-content'>
+					<div className='img-container'>
+						<Image src={aboutImage} alt='About' fill={true} />
+					</div>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+					</p>
+				</div>
 			</div>
 		</section>
 	);
